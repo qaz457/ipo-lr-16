463bbbb216ee6d6f015e211e417a5ded4e500e3f
Andrey <musik.09@list.ru>
2026-03-11 23:56:00 +0300

    add /login and /logout

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.base, name='base'),
@@ -14,4 +15,7 @@ urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('register/',views.register,name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
